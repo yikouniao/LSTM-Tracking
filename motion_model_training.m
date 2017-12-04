6098cdf9fdc9a61b8d18ba10360a39546d8ced0e
yikouniao <chinoandtippi@gmail.com>
2017-12-04 10:06:49 +0800

    motion model update

--- motion_model_training.m
+++ motion_model_training.m
@@ -5,6 +5,12 @@ foldername = ['MOT17-02-FRCNN'; 'MOT17-04-FRCNN'; 'MOT17-05-FRCNN';
 resolution = [[1920, 1080]; [1920, 1080]; [640, 480]; [1920, 1080];...
     [1920, 1080]; [1920, 1080]; [1920, 1080]];
 fps = [30; 30; 14; 30; 30; 30; 25];
+factor = 6; % keep the calculated velocities not too small
+
+v_x_train = [];
+v_y_train = [];
+v_x_test = [];
+v_y_test = [];
 
 seqnum = size(foldername, 1);
 for i = 1:seqnum
@@ -19,13 +25,13 @@ for i = 1:seqnum
         dets(:, 4) + dets(:, 6) / 2];
 
     % scale the coordinates into [0, 1]
-    c(:, 3) = c(:, 3) / resolution(1, 1);
-    c(:, 4) = c(:, 4) / resolution(1, 2);
+    c(:, 3) = c(:, 3) * factor / resolution(i, 1);
+    c(:, 4) = c(:, 4) * factor / resolution(i, 2);
 
     % form the samples
     max_id = max(c(:, 2));
-    idv_x = zeros(1, 2, 6);
-    idv_y = zeros(2, 1);
+    idv_x = zeros(1, 6, 2);
+    idv_y = zeros(1, 2);
     sample_cnt = 1;
     for j = 1:max_id % for each id
         if sum(c(:, 2)==j) < 8 % we need 6 velocities for X and 1 for Y
@@ -34,16 +40,17 @@ for i = 1:seqnum
         idc = c(c(:, 2)==j, :);
         idv_x_tmp = [];
         for k = 1:6
-            idv_x_tmp = [idv_x_tmp, (idc(k + 1, 3:4) - idc(k, 3:4))'];
+            idv_x_tmp = [idv_x_tmp; idc(k + 1, 3:4) - idc(k, 3:4)];
         end
-        idv_y_tmp = (idc(8, 3:4) - idc(7, 3:4))';
+        idv_y_tmp = idc(8, 3:4) - idc(7, 3:4);
         idv_x(sample_cnt, :, :) = idv_x_tmp(:, :);
-        idv_y(:, sample_cnt) = idv_y_tmp;
+        idv_y(sample_cnt, :) = idv_y_tmp;
         sample_cnt = sample_cnt + 1;
         for k = 1:size(idc(:, 1)) - 8
-            idv_x_tmp = [idv_x_tmp(:, 2:6), idv_y_tmp];
+            idv_x_tmp = [idv_x_tmp(2:6, :); idv_y_tmp];
             idv_x(sample_cnt, :, :) = idv_x_tmp;
-            idv_y(:, sample_cnt) = (idc(k + 8, 3:4) - idc(k + 7, 3:4))';
+            idv_y_tmp = idc(k + 8, 3:4) - idc(k + 7, 3:4);
+            idv_y(sample_cnt, :) = idv_y_tmp;
             sample_cnt = sample_cnt + 1;
         end
     end
@@ -52,14 +59,24 @@ for i = 1:seqnum
     save(['../train/' foldername(i, :) '/v/v_x.mat'], 'idv_x');
     save(['../train/' foldername(i, :) '/v/v_y.mat'], 'idv_y');
 
-    split = fix(size(idv_y,2) * 0.7);
+    split = fix(size(idv_y, 1) * 0.7);
     idv_x_train = idv_x(1:split, :, :);
-    idv_y_train = idv_y(:, 1:split);
-    idv_x_test = idv_x(split + 1:size(idv_y,2), :, :);
-    idv_y_test = idv_y(:, split + 1:size(idv_y,2));
+    idv_y_train = idv_y(1:split, :);
+    idv_x_test = idv_x(split + 1:size(idv_y, 1), :, :);
+    idv_y_test = idv_y(split + 1:size(idv_y, 1), :);
 
     save(['../train/' foldername(i, :) '/v/v_x_train.mat'], 'idv_x_train');
     save(['../train/' foldername(i, :) '/v/v_y_train.mat'], 'idv_y_train');
     save(['../train/' foldername(i, :) '/v/v_x_test.mat'], 'idv_x_test');
     save(['../train/' foldername(i, :) '/v/v_y_test.mat'], 'idv_y_test');
+    
+    v_x_train = [v_x_train; idv_x_train];
+    v_y_train = [v_y_train; idv_y_train];
+    v_x_test = [v_x_test; idv_x_test];
+    v_y_test = [v_y_test; idv_y_test];
 end
+
+save('../train/v/v_x_train.mat', 'v_x_train');
+save('../train/v/v_y_train.mat', 'v_y_train');
+save('../train/v/v_x_test.mat', 'v_x_test');
+save('../train/v/v_y_test.mat', 'v_y_test');
--- py/motion.py
+++ py/motion.py
@@ -5,28 +5,31 @@ from keras.models import Sequential
 from keras.layers import LSTM
 from keras.layers import Dense
 from keras import optimizers
+from keras import regularizers
 from scipy import io
 
-LRATE, DECAY = 0.002, 1e-5
+LRATE, DECAY = 1e-3, 2e-5
 OPTIMIZER, LOSS = 'adam', 'mean_squared_error'
-DIMS, LENGTH, LSTM_UNITS = 2, 6, 8
-EPOCHS, BATCH_SIZE, VERBOSE = 100, 64, 10
+DIMS, LENGTH, LSTM_UNITS = 2, 6, 20
+EPOCHS, BATCH_SIZE, VERBOSE = 50, 64, 2
 FPATH = '../../train/v/%s.mat'
 
-x_train = io.loadmat(FPATH % 'v_x_train')['idv_x_train']
-x_test = io.loadmat(FPATH % 'v_x_test')['idv_x_test']
-y_train = io.loadmat(FPATH % 'v_y_train')['idv_y_train']
-y_test = io.loadmat(FPATH % 'v_y_test')['idv_y_test']
+x_train = io.loadmat(FPATH % 'v_x_train')['v_x_train']
+x_test = io.loadmat(FPATH % 'v_x_test')['v_x_test']
+y_train = io.loadmat(FPATH % 'v_y_train')['v_y_train']
+y_test = io.loadmat(FPATH % 'v_y_test')['v_y_test']
 
 model = Sequential()
-model.add(LSTM(LSTM_UNITS, input_shape=(DIMS, LENGTH), unroll=True))
+model.add(LSTM(LSTM_UNITS, input_shape=(LENGTH, DIMS),
+               kernel_regularizer=regularizers.l2(1e-3),
+               recurrent_regularizer=regularizers.l2(1e-3), unroll=True))
 model.add(Dense(DIMS))
 
 m_adam = optimizers.Adam(lr=LRATE, beta_1=0.9, beta_2=0.999, epsilon=1e-08,
                          decay=DECAY)
 model.compile(optimizer=OPTIMIZER, loss=LOSS, metrics=['accuracy'])
 model.fit(x_train, y_train, epochs=EPOCHS, batch_size=BATCH_SIZE,
-          verbose=VERBOSE)
+          verbose=VERBOSE, validation_data=(x_test, y_test))
 score = model.evaluate(x_test, y_test, batch_size=BATCH_SIZE)
 
 model.save('m_model.h5')
